8937e60d5084d319bab6494ea970ef94eedbe4ed
Stephen Pratt <stevefsp@gmail.com>
2015-05-23 17:31:46 -0600

    Implement Custom Search Terms Editor
    Switches the mount point search term editor to a reorderable list.

--- Source/Lizitt/Outfitter/Editor/OutfitSearchTermsEditor.cs
+++ Source/Lizitt/Outfitter/Editor/OutfitSearchTermsEditor.cs
@@ -21,33 +21,225 @@
  */
 using com.lizitt.u3d.editor;
 using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
 
 namespace com.lizitt.outfitter.editor
 {
     [CustomEditor(typeof(OutfitSearchTerms))]
+    [CanEditMultipleObjects]
     public class OutfitSearchTermsEditor
         : Editor
     {
+        #region Menu Items
+
         private const string MenuItemName = "Outfit Search Terms";
         private const int MenuPriority = EditorUtil.AssetGroup + OutfitterEditor.MenuPriority + 2;
 
         [MenuItem(EditorUtil.AssetCreateMenu + MenuItemName, false, MenuPriority)]
         [MenuItem(EditorUtil.UnityAssetCreateMenu + MenuItemName, false, MenuPriority)]
-        static void CreateAsset()
+        private static void CreateAsset()
         {
             var item = EditorUtil.CreateAsset<OutfitSearchTerms>("", OutfitterEditor.AssetLabel);
             Selection.activeObject = item;
         }
 
+        #endregion
+
+        private const string MountListName = "m_MountTerms";
+        private const string ItemTypeName = "m_MountType";
+        private const string ItemTermName = "m_Term";
+
+        private struct Props
+        {
+            public SerializedProperty headTerm;
+            public SerializedProperty eyeTerm;
+            public SerializedProperty bodyTerm;
+            public SerializedProperty blendTerm;
+
+            public Props(SerializedObject sobj)
+            {
+                headTerm = sobj.FindProperty("m_HeadTerm");
+                eyeTerm = sobj.FindProperty("m_EyeTerm");
+                bodyTerm = sobj.FindProperty("m_BodyTerm");
+                blendTerm = sobj.FindProperty("m_BlendHeadTerm");
+            }
+        }
+
         public override void OnInspectorGUI()
         {
-            base.OnInspectorGUI();
+            serializedObject.Update();
+
+            var props = new Props(serializedObject);
+
+            EditorGUILayout.LabelField("Material Terms", EditorGUIUtil.BoldLabel);
+
+            EditorGUILayout.PropertyField(props.headTerm);
+            EditorGUILayout.PropertyField(props.eyeTerm);
+            EditorGUILayout.PropertyField(props.bodyTerm);
+
+            EditorGUILayout.Separator();
+            EditorGUILayout.LabelField("Renderer Terms", EditorGUIUtil.BoldLabel);
+
+            EditorGUILayout.PropertyField(props.blendTerm);
+
+            DrawMountPointTerms();
+
+            if (UnityEngine.GUI.changed)
+            {
+                UnityEngine.Debug.Log("change");
+                serializedObject.ApplyModifiedProperties();
+            }
 
             EditorGUILayout.Separator();
 
-            EditorGUILayout.HelpBox("Terms are used to perform partial case-insenstive matches of"
-                + " object names in an outfit prototype.  The type of object is dependant on the term.",
-                    MessageType.Info, true);
+            EditorGUILayout.HelpBox("Terms are used to perform partial, case-insenstive matches of"
+                + " object names in an outfit prototype.  The type of object is dependant on the"
+                + " term.", 
+                MessageType.Info, true);
+        }
+
+        private ReorderableList m_List;
+
+        private void DrawMountPointTerms()
+        {
+            EditorGUILayout.Separator();
+            EditorGUILayout.LabelField("Mount Points", EditorGUIUtil.BoldLabel);
+
+            if (serializedObject.isEditingMultipleObjects)
+            {
+                EditorGUILayout.HelpBox("Can't edit mount point terms while in multi-edit mode.",
+                    MessageType.Warning, true);
+                return;
+            }
+
+            if (m_List == null)
+                CreateReorderableList();
+
+            m_List.DoLayoutList();
+        }
+
+        private void CreateReorderableList()
+        {
+            var list = new ReorderableList(serializedObject
+                , serializedObject.FindProperty(MountListName)
+                , true, true, true, true);
+
+            list.drawHeaderCallback = delegate(Rect rect)
+            {
+                EditorGUI.LabelField(rect, "Mount Point Terms");
+            };
+
+            list.drawElementCallback =
+                delegate(Rect position, int index, bool isActive, bool isFocused)
+                {
+                    var element = list.serializedProperty.GetArrayElementAtIndex(index);
+
+                    var typProp = element.FindPropertyRelative(ItemTypeName);
+                    var termProp = element.FindPropertyRelative(ItemTermName);
+
+                    GUIContent label;
+
+                    bool hasError = false;
+                    if (typProp.enumValueIndex == -1)
+                    {
+                        label = new GUIContent("Invalid Type", "Enum type value changed or removed?");
+                        hasError = true;
+                    }
+                    else
+                        label = new GUIContent(typProp.enumDisplayNames[typProp.enumValueIndex]);
+
+                    var rect = new Rect(position.x, 
+                        position.y + EditorGUIUtility.standardVerticalSpacing,
+                        90, EditorGUIUtility.singleLineHeight);
+
+                    GUIStyle style = GUI.skin.label;
+
+                    if (hasError)
+                        style = EditorGUIUtil.RedLabel;
+
+                    EditorGUI.LabelField(rect, label, style);
+
+                    rect = new Rect(
+                        rect.xMax + 5, rect.y, position.width - rect.width - 5, rect.height);
+
+                    EditorGUI.PropertyField(rect, termProp, GUIContent.none);
+                };
+
+            GenericMenu.MenuFunction2 addItem = delegate(object typValue)
+            {
+                list.index = AddItem(list.serializedProperty, (int)typValue);
+            };
+
+            GenericMenu.MenuFunction addAllItems = delegate()
+            {
+                var stdNames = OutfitterEditorUtil.MountPointTypeNames;
+                var stdValues = OutfitterEditorUtil.MountPointTypeValues;
+
+                var idx = list.serializedProperty.arraySize;
+
+                for (int i = 0; i < stdNames.Length; i++)
+                {
+                    if (!IsInList(list.serializedProperty, stdValues[i]))
+                        AddItem(list.serializedProperty, stdValues[i]);
+                }
+
+                list.index = idx;  // Assumes never called if list is full.
+            };
+
+            list.onAddDropdownCallback = delegate(Rect rect, ReorderableList roList)
+            {
+                var menu = new GenericMenu();
+
+                var stdNames = OutfitterEditorUtil.MountPointTypeNames;
+                var stdValues = OutfitterEditorUtil.MountPointTypeValues;
+
+                for (int i = 0; i < stdNames.Length; i++)
+                {
+                    if (!IsInList(list.serializedProperty, stdValues[i]))
+                        menu.AddItem(new GUIContent(stdNames[i]), false, addItem, stdValues[i]);
+                }
+
+                if (menu.GetItemCount() == 0)
+                    menu.AddDisabledItem(new GUIContent("[None Available]"));
+                else
+                    menu.AddItem(new GUIContent("All"), false, addAllItems);
+
+                menu.ShowAsContext();
+            };
+
+            m_List = list;
+        }
+
+        private int AddItem(SerializedProperty listProp, int typValue)
+        {
+            int nidx = listProp.arraySize;
+
+            listProp.arraySize++;
+
+            var element = listProp.GetArrayElementAtIndex(nidx);
+            element.FindPropertyRelative(ItemTypeName).intValue = typValue;
+            element.FindPropertyRelative(ItemTermName).stringValue = ((MountPointType)typValue).ToString();
+
+            listProp.serializedObject.ApplyModifiedProperties();
+
+            return nidx;
+        }
+
+        private bool IsInList(SerializedProperty listProp, int typValue)
+        {
+            // Hack.
+            if (typValue == (int)MountPointType.Root)
+                return true;
+
+            for (int i = 0; i < listProp.arraySize; i++)
+            {
+                var element = listProp.GetArrayElementAtIndex(i);
+                if (element.FindPropertyRelative(ItemTypeName).intValue == typValue)
+                    return true;
+            }
+
+            return false;
         }
     }
 }

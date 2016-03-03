93bc4b59bca6bdfa21ec192f819cc196a87660d5
Stephen Pratt <stevefsp@gmail.com>
2016-03-02 21:03:49 -0700

    Initial check-in: Body Prototype Manager components.  (Re-try)

---
+++ Source/Plugins/Lizitt/Outfitter/Prototyping/Editor/BodyPrototyperManagerEditor.cs
@@ -0,0 +1,32 @@
+﻿#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+using com.lizitt.editor;
+
+namespace com.lizitt.outfitter.proto.editor
+{
+    [CustomEditor(typeof(BodyPrototyperManager))]
+    public class BodyPrototyperManagerEditor
+        : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            EditorGUIUtil.BeginLabelWidth(115);
+            base.OnInspectorGUI();
+            EditorGUIUtil.EndLabelWidth();
+
+            EditorGUILayout.Space();
+
+            var targ = target as BodyPrototyperManager;
+
+            var prefix = targ.InvertButtons
+                ? "Mouse0: Rotate camera.\nMouse1: Rotate body."
+                : "Mouse0: Rotate body.\nMouse1: Rotate camera.";
+
+            EditorGUILayout.HelpBox(prefix + "\nMouse2: Camera height.\nWheel: Camera distance.", MessageType.None);
+        }
+    }
+}
+
+#endif
---
+++ Source/Plugins/Lizitt/Outfitter/Prototyping/Editor/BodyPrototyperSettingsEditor.cs
@@ -0,0 +1,37 @@
+﻿#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+
+namespace com.lizitt.outfitter.proto.editor
+{
+    [CustomEditor(typeof(BodyPrototyperSettings))]
+    public class BodyPrototyperSettingsEditor
+        : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUILayout.Space();
+
+            var targ = target as BodyPrototyperSettings;
+
+            if (Application.isPlaying)
+                return;
+
+            if (!targ.Body)
+            {
+                if (GUILayout.Button("Instantiate a Standard Body"))
+                    targ.InitializeBody();
+            }
+
+            EditorGUILayout.Space();
+
+            if (targ.gameObject.tag != DefaultTag.EditorOnly)
+                EditorGUILayout.HelpBox("The GameObject tag must be '" + DefaultTag.EditorOnly + "'", MessageType.Error);
+        }
+    }
+}
+
+#endif
